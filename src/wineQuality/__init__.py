6cddd2bea272425b91cd7e89374ba5da65c4e5a3
Harsha Naik <naikharshas235@gmail.com>
2024-04-06 22:29:39 +0530

    warnings are removed

--- src/wineQuality/__init__.py
+++ src/wineQuality/__init__.py
@@ -12,7 +12,6 @@ os.makedirs(log_dir, exist_ok=True)
 logging.basicConfig(
     level=logging.INFO,
     format=logging_str,
-   
     handlers=[
         logging.FileHandler(log_filepath),
         logging.StreamHandler(sys.stdout)
