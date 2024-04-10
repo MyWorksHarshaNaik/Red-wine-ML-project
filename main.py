55d4a4ee712b8fcbab0f8d505ce42e5078372616
Harsha Naik <naikharshas235@gmail.com>
2024-04-10 15:30:07 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from wineQuality import logger
+from wineQuality.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline # noqa
 
-logger.info("This is our custom log!")
+STAGE_NAME = "Data Ingestion Stage"
+
+try:
+    logger.info(f">>> stage {STAGE_NAME} started <<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>> stage {STAGE_NAME} completed <<<\n\nx=====x")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/wineQuality/components/data_ingestion.py
@@ -0,0 +1,28 @@
+import os
+import urllib.request as request
+import zipfile
+from wineQuality import logger
+from wineQuality.utils.common import get_size
+from wineQuality.entity.config_entity import DataIngestionConfig
+from pathlib import Path
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+    def download_file(self):
+        if not os.path.exists(self.config.local_data_file):
+            filename, headers = request.urlretrieve(
+                url=self.config.source_URL,
+                filename=self.config.local_data_file
+            )
+            logger.info(f"{filename} download! with info: \n{headers}")
+        else:
+            logger.info(f"File exists of size: {get_size(Path(self.config.local_data_file))}") # noqa
+
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
